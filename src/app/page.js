8f78dbcf50779dfbb05e3d0b8633d4eaea425bf9
Karthik RP <blendedidea@gmail.com>
2024-05-16 21:41:51 +0530

    Info Page
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -4,91 +4,33 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+      <div className={styles.container}>
         <Image
+          src="/logo.png"
+          alt="Nisarga Eco Consultants"
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={750}
+          height={250}
           priority
         />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        <div className={styles.info}>
+          <p className={styles.infoHeader}>
+            Where Sustainability Thrives!
           </p>
-        </a>
+          <p className={styles.infoText}>
+            Get ready to embark on a journey of regeneration with Us.
+            We're gearing up to revolutionize the way you interact with the environment, offering a suite of services designed to breathe new life into your eco-practices.
+          </p>
+
+          <p className={styles.infoText}>
+            We offer comprehensive solutions for Rainwater Harvesting, Green Audit, Energy Audit, Environment Audit, and Composting.
+          </p>
+
+          <p className={styles.infoText}>Stay tuned as we prepare to launch our website. </p>
+        </div>
+
       </div>
     </main>
   );
