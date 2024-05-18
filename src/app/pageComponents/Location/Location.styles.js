add0d11ce73bb69d4799b7dcf7c81e3a1537ba99
Karthik RP <blendedidea@gmail.com>
2024-05-18 21:48:49 +0530

    Spacing for location drop
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

--- src/app/pageComponents/Location/Location.styles.js
+++ src/app/pageComponents/Location/Location.styles.js
@@ -2,5 +2,11 @@ import { styled } from "styled-components";
 
 export const LocationDrop = styled.iframe`
     border: 0;
-    margin-top: 3rem;
+    margin-top: 4rem;
+    margin-bottom: 4rem;
+
+    @media (min-width: 500px) {
+        margin-top: 10rem;
+        margin-bottom: 10rem;
+    }
 `;
--- src/app/pageComponents/Location/index.js
+++ src/app/pageComponents/Location/index.js
@@ -7,7 +7,7 @@ export default function Location() {
         <LocationDrop
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3837.6442913336646!2d74.5420268!3d15.875282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf67a6812b5e7b%3A0x7675d026ed1c05aa!2sNisarga%20Eco%20Consultants!5e0!3m2!1sen!2sin!4v1716048290560!5m2!1sen!2sin"
             width="100%"
-            height="300"
+            height="400"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade" />
