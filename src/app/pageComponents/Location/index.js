e98b51e468fcf502195d4b4b4b2238e641f2fa6c
Karthik RP <blendedidea@gmail.com>
2024-05-18 21:36:15 +0530

    About Page
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

--- src/app/pageComponents/Location/index.js
+++ src/app/pageComponents/Location/index.js
@@ -5,7 +5,7 @@ import { LocationDrop } from './Location.styles';
 export default function Location() {
     return (
         <LocationDrop
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3837.622507893182!2d74.50954357531111!3d15.87642554457025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf614d71b867bd%3A0x4a8f6befe1b1517d!2sBasavaraj%20Complex!5e0!3m2!1sen!2sin!4v1716042522734!5m2!1sen!2sin"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3837.6442913336646!2d74.5420268!3d15.875282!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf67a6812b5e7b%3A0x7675d026ed1c05aa!2sNisarga%20Eco%20Consultants!5e0!3m2!1sen!2sin!4v1716048290560!5m2!1sen!2sin"
             width="100%"
             height="300"
             allowFullScreen=""
