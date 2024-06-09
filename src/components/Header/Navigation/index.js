3795c1096eae1900c9eaca171182de85828530f9
Karthik RP <blendedidea@gmail.com>
2024-06-09 20:39:52 +0530

    Nav menu
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { NavigationContainer, NavigationItem, NavigationLink, NavigationChildMenu } from './styles';
+'use client';
+import React, { useState } from 'react';
+import { NavigationContainer, NavigationItem, NavigationLink, NavigationChildMenu, NavToggle, CloseButton } from './styles';
 import { SERVICES } from '@/app/services/[slug]/service.constants';
+import Image from 'next/image';
 
 export default function Navigation() {
+    const [display, setDisplay] = useState(false);
+
     const navigationItems = [
         {
             name: 'About',
@@ -17,24 +21,35 @@ export default function Navigation() {
             }))
         },
     ]
+
+    const toggleMenu = () => {
+        setDisplay(d => !d);
+    };
+
     return (
-        <NavigationContainer>
-            {
-                navigationItems.map(ni => (
-                    <NavigationItem key={ni.name}>
-                        <NavigationLink href={ni.link}> {ni.name}</NavigationLink>
-                        {
-                            ni.children ? (
-                                <NavigationChildMenu>
-                                    {ni.children.map(link =>
-                                        <NavigationLink href={link.url} key={link.url}> {link.name}</NavigationLink>)
-                                    }
-                                </NavigationChildMenu>
-                            ) : null
-                        }
-                    </NavigationItem>
-                ))
-            }
-        </NavigationContainer>
+        <>
+            <NavToggle onClick={() => setDisplay(true)}><Image alt="Close" src="/icons/hamburger.svg" height={20} width={20} /></NavToggle>
+            <NavigationContainer display={display.toString()}>
+                <CloseButton onClick={() => setDisplay(false)}>
+                    <Image alt="Close" src="/icons/close.svg" height={20} width={20} />
+                </CloseButton>
+                {
+                    navigationItems.map(ni => (
+                        <NavigationItem key={ni.name}>
+                            <NavigationLink href={ni.link} onClick={() => setDisplay(false)}> {ni.name}</NavigationLink>
+                            {
+                                ni.children ? (
+                                    <NavigationChildMenu>
+                                        {ni.children.map(link =>
+                                            <NavigationLink href={link.url} key={link.url} onClick={() => setDisplay(false)}> {link.name}</NavigationLink>)
+                                        }
+                                    </NavigationChildMenu>
+                                ) : null
+                            }
+                        </NavigationItem>
+                    ))
+                }
+            </NavigationContainer>
+        </>
     )
 }
