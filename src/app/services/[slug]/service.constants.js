14306529da8b6b02513ca25f8fc0b88114bf7921
Karthik RP <blendedidea@gmail.com>
2024-06-09 17:03:45 +0530

    services new
    Signed-off-by: Karthik RP <blendedidea@gmail.com>

---
+++ src/app/services/[slug]/service.constants.js
@@ -0,0 +1,123 @@
+export const SERVICES = Object.freeze([
+    {
+        slug: 'rain-water-harvesting',
+        meta: {
+            title: 'Rain Water Harvesting',
+            description: 'Explore rainwater harvesting solutions in Bengaluru, Karnataka, India on our website. Discover innovative techniques and reliable resources to promote water sustainability and combat water scarcity effectively'
+        },
+        bgImage: "",
+        info: {
+            title: "Rain Water Harvesting",
+            subTitle: "Effective Solutions",
+            icon: "/services/icons/rwh.svg",
+            description: [
+                `Rainwater harvesting (RWH) presents a straightforward approach to collect rainfall for future use.
+                As groundwater levels diminish and climate patterns become increasingly erratic, 
+                RWH serves as a crucial strategy to alleviate these challenges. By capturing rainwater,
+                communities can replenish local aquifers, mitigate urban flooding, and ensure water availability, 
+                particularly in regions facing scarcity.`,
+
+                `The Nisarga RWH Unit boasts a distinctive design that is both compact 
+                and eliminates the need for excavation or extensive civil works on-site. Installation requires only minor
+                adjustments to the plumbing network and minimal time, making it suitable for various settings such as 
+                residential plots, apartments, educational institutions, corporate offices, and industries. Utilizing filter 
+                media that mimics natural riverbeds, our RWH unit offers an eco-friendly solution.`
+            ],
+        }
+    },
+
+    {
+        slug: 'sewage-treatment-plants',
+        meta: {
+            title: 'Sewage Treatment Plants',
+            description: 'Experience our expertise in Sewage Treatment Plants, offering design, equipment supply, and operations and maintenance services. Safely release effluent through effective sewage treatment techniques. Bengaluru, Karnataka, India'
+        },
+        bgImage: "",
+        info: {
+            title: "Sewage Treatment Plants",
+            subTitle: "Innovative Approaches",
+            icon: "/services/icons/stp.svg",
+            description: [
+                `Sewage, originating from domestic sources, comprises a mix of liquid and semi-solid waste with various 
+                physical, chemical, and biological components. To prevent environmental harm upon discharge, effective sewage
+                treatment is essential. This process involves a combination of physical, chemical, and biological techniques
+                to produce an effluent that can be safely released.`,
+
+                `We specialize in state-of-the-art Sewage Treatment Plants, offering comprehensive services such as design,
+                equipment supply, and operations and maintenance for both new and existing plants.`
+            ],
+        }
+    },
+
+    {
+        slug: 'green-audit',
+        meta: {
+            title: 'Green audit',
+            description: 'Enhance sustainability with Green, Energy, and Environment Audits. Transform campuses into eco-friendly spaces with waste management, energy conservation, and green initiatives. Bengaluru, Karnataka, India'
+        },
+        bgImage: "",
+        info: {
+            title: "Green audit",
+            subTitle: "Sustainable Solutions",
+            icon: "/services/icons/ga.svg",
+            description: [
+                `Green, Energy, and Environment audits serve to evaluate the current environmental status of educational 
+                institutions, companies, and factories. Through this process, campuses can be transformed into more 
+                eco-friendly environments. Initiatives such as waste management, water quality assessment, energy conservation,
+                and enhancing green spaces contribute to making the client's campus more sustainable.`,
+
+                `Incorporating Green, Energy, and Environment Audits into Criteria 7 of the NAAC (National Assessment and 
+                Accreditation Council) underscores their significance in assessing and accrediting educational institutions
+                across India`,
+
+                `At our ISO 9001:2015, ISO 17020:2012, and ISO 14001:2015 certified firm, we conduct thorough Green, Energy, 
+                and Environment Audits, tailoring sustainable solutions to meet the specific needs of our clients.`
+            ],
+        }
+    },
+
+    {
+        slug: 'composting',
+        meta: {
+            title: 'Composting',
+            description: 'Nisarga Eco Consultants introduces cutting-edge Aerobic composting technology for efficient decomposition of food waste and plant matter. Reduce your carbon footprint with our eco-friendly solution! Bengaluru, Karnataka, India'
+        },
+        bgImage: "",
+        info: {
+            title: "Composting",
+            subTitle: "Wet Waste Solutions",
+            icon: "/services/icons/composting.svg",
+            description: [
+                `Composting entails the organic decomposition of materials like food waste and plant matter through biological processes, 
+                facilitated either aerobically (in the presence of oxygen) or anaerobically (in the absence of oxygen) under controlled 
+                conditions. This transformative process yields odorless end products, commonly referred to as compost, humus, or manure.
+                Compost serves as a valuable soil conditioner, enriching soil with organic carbon and eliminating the need for chemical 
+                fertilizers, thereby reducing the carbon footprint.`,
+
+                `At Nisarga Eco Consultants, we've pioneered an Aerobic composting technology capable of composting wet waste
+                 within 35-50 days. This innovative approach is suitable for both individual households and large-scale waste generators.`,
+
+                `At our ISO 9001:2015, ISO 17020:2012, and ISO 14001:2015 certified firm, we conduct thorough Green, Energy, 
+                and Environment Audits, tailoring sustainable solutions to meet the specific needs of our clients.`
+            ],
+        }
+    },
+
+    {
+        slug: 'workshops',
+        meta: {
+            title: 'Workshops',
+            description: 'Nisarga Eco Consultants introduces cutting-edge Aerobic composting technology for efficient decomposition of food waste and plant matter. Reduce your carbon footprint with our eco-friendly solution! Bengaluru, Karnataka, India'
+        },
+        bgImage: "",
+        info: {
+            title: "Workshops",
+            subTitle: "Learn to save",
+            icon: "/services/icons/composting.svg",
+            description: [
+                `We organize workshops on sustainable living, nature based sewage treatment units, composting, bioenzymes,
+                 rain water harvesting, plastic waste management and many more.`
+            ],
+        }
+    },
+]);
---
+++ src/app/services/[slug]/styles.js
@@ -0,0 +1,51 @@
+'use client';
+import { Container } from "@/components/Container";
+import styled from "styled-components";
+
+export const Banner = styled.div`
+    min-height: 40vh;
+    background-image: url("/services/banner.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const BannerContent = styled.h1`
+    font-size: 3rem;
+    font-weight: 400;
+    color: var(--color-white);
+    text-align: center;
+`;
+
+export const CustomContainer = styled(Container)`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const InfoContainer = styled.div`
+    margin: 5rem 0;
+    background-color: #F2F6F4;
+    padding: 1.5rem 2rem;
+    border-radius: 2rem;
+`;
+
+export const InfoHeading = styled.div`
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    font-size: 2rem;
+    font-weight: 700;
+
+    padding-bottom: 2rem;
+`;
+
+export const Info = styled.div`
+    font-size: 1.25rem;
+    text-align: center;
+    margin-bottom: 1rem;
+`;
